5a4e2fc2639bae9fcd66412c5fde5bf0c9cc43bc
alromav1120 <alromav1120@gmail.com>
2015-07-13 11:27:22 -0500

    Factorial

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Factorial
+{
+    class Program
+    {
+        public int factorial(int a) {
+           
+            int f = 1;
+
+            for (int x = 1; x <= a; x++) {
+                f = f * x;
+            }
+
+            Console.Write("El factorial es: {0}",f.ToString());
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            try {
+
+                int a;
+                Program O = new Program();
+                Console.Write("Dame un numero: ");
+                a = Convert.ToInt32(Console.ReadLine());
+                O.factorial(a);
+                Console.ReadKey();
+            }
+            catch { 
+            
+            }
+        }
+    }
+}
+
